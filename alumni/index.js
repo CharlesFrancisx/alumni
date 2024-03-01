a276b9960e2670dc9f84acc6e57f3b8faf38d1e6
charles francis <104983548+CharlesFrancisx@users.noreply.github.com>
2024-03-01 23:54:10 +0800

    Add files via upload

---
+++ alumni/index.js
@@ -0,0 +1,34 @@
+let signup = document.querySelector("#signup");
+let signin = document.querySelector("#signin");
+
+
+
+signin.addEventListener("click",()=>{
+
+  
+signin.style.background="#25106d";
+signin.style.color="#fff";
+
+
+signup.style.background="#fff";
+signup.style.color="#000";
+
+
+
+
+
+});
+
+
+signup.addEventListener("click",()=>{
+
+  
+    signin.style.background="#fff";
+    signin.style.color="#000";
+    
+    
+    signup.style.background="#25106d";
+    signup.style.color="#fff";
+    
+});
+    
